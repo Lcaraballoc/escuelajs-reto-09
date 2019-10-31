ecd23fd8d211b943b436ace42f2562964803dedd
Luis Caraballo <luisred61@gmail.com>
2019-10-31 14:19:25 -0500

    1st and 2nd challenge done

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,11 @@ const receipt = '../assets/receipt.pdf'
 
 const platziStore = (app) => {
   const router = express.Router();
-  app.use('/api/', router);
+  app.use('/api/products', router);
 
   const productService = new ProductService();
 
-  router.get('/', (req, res) => {
+  router.get('/api', (req, res) => {
     res.send(`API v2`);
   });
 
@@ -18,9 +18,71 @@ const platziStore = (app) => {
     res.sendFile(file);
   });
 
-  router.get('/products', async (req, res, next) => {
+  router.get('/', async (req, res, next) => {
     const storeProducts = await productService.getProducts()
     res.status(200).json(storeProducts);
+  });
+
+  router.post('/', async function (req, res, next) {
+    const { body: product } = req;
+    try {
+      const createProductId = await productService.createProduct({ product });
+      res.status(201).json({
+        data: createProductId,
+        message: 'product created'
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  router.get("/:productId", async function (req, res, next) {
+    const { productId } = req.params;
+    try {
+      const products = await productService.getProduct({ productId })
+
+      res.status(200).json({
+        data: products,
+        message: 'movie retreived'
+      })
+
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.put("/:productId", async function (req, res, next) {
+    const { productId } = req.params;
+    const { body: product } = req;
+    try {
+      const updatedProductId = await productService.updateProduct({
+        productId,
+        product
+      })
+
+      res.status(200).json({
+        data: updatedProductId,
+        message: 'product updated'
+      })
+
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.delete("/:productId", async function (req, res, next) {
+    const { productId } = req.params;
+    try {
+      const deletedProductId = await productService.deleteProduct({ productId })
+
+      res.status(200).json({
+        data: deletedProductId,
+        message: 'product deleted'
+      })
+
+    } catch (err) {
+      next(err);
+    }
   });
 
   router.get('*', (req, res) => {
